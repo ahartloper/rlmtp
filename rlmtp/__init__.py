fba9406c355edafbb8835954bb07b1cc10af02cb
ahartloper <alexander.hartloper@epfl.ch>
2019-09-11 13:52:23 +0200

    Add clean_data to package

--- rlmtp/__init__.py
+++ rlmtp/__init__.py
@@ -4,3 +4,4 @@ from .sync_video import dion7_times_to_video_times, output_frames_at_times
 from .create_latex_photos import latex_photo_compiler
 from .processing import process_specimen_data
 from .construct_database import write_description_database_csv
+from .filtering import clean_data
