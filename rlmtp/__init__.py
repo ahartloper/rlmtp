30184463076574964309d9809bb79936d9b68c86
ahartloper <alexander.hartloper@epfl.ch>
2020-02-25 14:44:50 +0100

    Add plotter to rlmtp import.

--- rlmtp/__init__.py
+++ rlmtp/__init__.py
@@ -4,3 +4,4 @@ from .sync_video import dion7_times_to_video_times, output_frames_at_times
 from .create_latex_photos import latex_photo_compiler
 from .processing import process_specimen_data
 from .construct_database import write_description_database_csv
+from .plotting import stress_strain_plotter, temp_time_plotter, temp_strain_plotter
