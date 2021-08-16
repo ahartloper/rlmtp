dc41770cd49db0e362429a79b0bf9519ff9a792f
ahartloper <alexander.hartloper@epfl.ch>
2021-08-16 12:02:54 +0100

    Update the version

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='rlmtp',
-      version='0.5.1',
+      version='0.6.0',
       description='RESSLab tools for material coupon test post-processing',
-      url='https://c4science.ch/source/rlmtp/',
+      url='https://github.com/ahartloper/rlmtp',
       author='ahartloper',
       author_email='alexander.hartloper@epfl.ch',
       license='MIT',
